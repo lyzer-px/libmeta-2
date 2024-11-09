abfae776c77fe9033019d435665640945f1e8490
Liam Harfouche <liam.harfouche@epitech.eu>
2024-11-10 00:13:54 +0100

    [ADD] : exit failure code

---
+++ modules/meta_libc/include/meta_libc.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** meta_libc
+** File description:
+** meta_libc.h
+*/
+
+#ifndef META_LIBC
+    #define META_LIBC
+    #ifndef META_EXTFAIL
+        #define META_EXTFAIL 84
+    #endif
+#endif
---
+++ modules/meta_libc/include/meta_libc_macros.h
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** meta_libc [WSL: Ubuntu]
+** File description:
+** meta_macros.h
+*/
+
+#ifndef META_MARCOS
+    #define META_MARCOS
+    #include <limits.h>
+    #ifndef IS_ALPHA
+        #define IS_ALPHA(c) ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    #endif
+    #ifndef IS_NUM
+        #define IS_NUM(nb) (nb >= '0' && nb <= '9')
+    #endif
+    #ifndef IS_ALPHANUM
+        #define IS_ALPHANUM(c) IS_ALPHA(c) || IS_NUM(c)
+    #endif
+    #ifndef OVERFLOW
+        #define OVERFLOW(nb) (long)nb < (long)INT_MIN || (long)nb > (long)INT_MAX
+    #endif
+#endif
